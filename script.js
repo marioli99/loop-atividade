4e8881695b2b3f66fae35220a95777267959ca19
marioli99 <mazmariana.mo@gmail.com>
2022-01-19 12:31:16 -0300

    Atualização - pergunta 1

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+
+for (let i=1; i <= 10; i++){
+    console.log(i)
+}
+
 //2 - Criar um loop que conte de 10 até 1 usando WHILE
 
 for (let i = 10; i >= 1; i--){
